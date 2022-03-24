085ff2b09eb88cdfeae4dcf23c32bbeed21fa1b0
Christian Lazo <christian@teamairship.com>
2022-03-24 17:52:06 -0600

    Add totals to itemized list, this will include correct total for sale items

--- lib/price_calculator.rb
+++ lib/price_calculator.rb
@@ -6,6 +6,28 @@ class PriceCalculator
   def initialize(grocery_list)
     @grocery_list  = grocery_list.split(',').map(&:strip).sort
     @itemized_list = create_itemized_list
+
+    perform
+  end
+
+  def perform
+    calculate_item_total
+  end
+
+  def calculate_item_total
+    itemized_list.each do |item, quantity|
+      normal_quantity = quantity
+      sale_quantity = 0
+
+      if sale_items.keys.include?(item.to_sym)
+        normal_quantity = quantity % sale_items.fetch(item.to_sym, 0)[0]
+        sale_quantity = quantity - normal_quantity
+      end
+
+      total = normal_quantity * store_items.fetch(item.to_sym, 0) + sale_quantity * sale_items.fetch(item.to_sym, 0)[1]
+
+      itemized_list[item] = [quantity, total]
+    end
   end
 
   # can use tally method in the future to make this process easier
@@ -13,6 +35,22 @@ class PriceCalculator
     grocery_list.each_with_object(Hash.new(0)) do |item, items|
       items[item] += 1
     end
+  end
+
+  def store_items
+    {
+      apple: 0.89,
+      banana: 0.99,
+      bread: 2.17,
+      milk: 3.97
+    }
+  end
+
+  def sale_items
+    {
+      bread: [3, 2.00],
+      milk: [2, 2.50]
+    }
   end
 end
 
